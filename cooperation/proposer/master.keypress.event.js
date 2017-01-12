20680594fa076ce13509ecd5a33f72d8838bddd5
AfterStories <609449489@qq.com>
2017-01-12 14:13:06 +0800

    2017-1-12    14：10
    2017-1-12    14：10

---
+++ cooperation/proposer/master.keypress.event.js
@@ -0,0 +1,38 @@
+﻿// disable submit on press enter (only applied to text and password field)
+$(document).keypress(function (evt) {
+    evt = (evt) ? evt : ((event) ? event : null);
+    var node = (evt.target) ? evt.target : ((evt.srcElement) ? evt.srcElement : null);
+    if ((evt.keyCode == 13) && (node.type == "text" || node.type == "password")) {
+        if (node.id == "UserName" && node.value) {
+            if ($("#Password").val()) {
+                $("#login .button").click();
+            } else {
+                $("#Password").focus();
+            }
+        }
+        if (node.id == "Password" && node.value) {
+            if ($("#UserName").val()) {
+                $("#login .button").click();
+            } else {
+                $("#UserName").focus();
+            }
+        }
+        if (node.id == "txtUsn" && node.value) {
+            if ($("#txtPwd").val()) {
+                $(".PL_login_btn .PL_button").click();
+            } else {
+                $("#txtPwd").focus();
+            }
+        }
+        if (node.id == "txtPwd" && node.value) {
+            if ($("#txtUsn").val()) {
+                $(".PL_login_btn .PL_button").click();
+            } else {
+                $("#txtUsn").focus();
+            }
+        }
+        return false;
+    }
+    return true;
+});
+
---
+++ cooperation/withdraw/master.dialogmanager.js
@@ -0,0 +1,34 @@
+﻿    function DialogManager_isLogin(memberCode) 
+    {
+        if (memberCode == "") { return false; }
+        else { return true; }
+    }
+
+
+    function DialogManager_showLoginFail(dialogTitle, dialogMessage) 
+    {
+        dialogHtmlMessage = "<p>" + dialogMessage + "</p>";
+
+        DialogManager_Show(dialogTitle, dialogHtmlMessage);
+
+    }
+
+
+    function DialogManager_Show(dialogTitle, dialogHtmlMessage) 
+    {
+       //$("#dialog-message").html("Test change content msg"); 
+        $("#dialog-message").html(dialogHtmlMessage);
+        //$("#dialog-message").html('<%=GetGlobalResourceObject("Common", "PleaseLogin").ToString()%>');
+        $("#dialog-message").dialog({
+            title: dialogTitle,
+            //text: "abc",
+            modal: true,
+            buttons: {
+                Ok: function () {
+                    $(this).dialog("close");
+                }
+            }
+           
+        });
+        
+    }
